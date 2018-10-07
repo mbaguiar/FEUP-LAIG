5d4bd285c7d740b12d5a5324bc252d3cf28a0ded
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2018-10-07 18:41:12 +0100

    Fixed cylinder and almost done xml scene

--- demo/primitives/NoBaseCylinder.js
+++ demo/primitives/NoBaseCylinder.js
@@ -10,7 +10,6 @@ class NoBaseCylinder extends CGFobject {
 		this.top = top;
 		this.base = base;
 		this.height = height;
-		//this.texCoords = [];
 		this.initBuffers();
 	};
 
@@ -18,59 +17,48 @@ class NoBaseCylinder extends CGFobject {
 		this.vertices = [];
 		this.indices = [];
 		this.normals = [];
-		//this.texCoords = [];
+		this.texCoords = [];
 
-		let ang = 0;
-		let angDelta = Math.PI * 2 / this.slices;
-
-		let z = 0;
-		let zDelta = this.height / this.stacks;
-
+		let delta_height = this.height / this.stacks;
 		let delta = (this.top - this.base) / this.stacks;
+		let angDelta = - Math.PI * 2 / this.slices;
 
-		z = 0
-		for (let j = 0; j < this.stacks; j++) {
+		let incX = 1 / this.slices;
+		let incY = 1 / this.stacks;
+
+		let x = 0;
+		let y = 0;
+
+
+		for (let j = 0; j <= this.stacks; j++) {
 
 			let inc = j * delta + this.base;
 
+			for (let i = 0; i <= this.slices; i++) {
+				this.vertices.push(inc * Math.cos(i * angDelta), inc * Math.sin(i * angDelta), j* delta_height);
+				this.normals.push(Math.cos(i * angDelta), Math.sin(i * angDelta), 0);
+
+				this.texCoords.push(x, y);
+				x+= incX;
+			}
+
+			x = 0;
+			y += incY;
+		}
+
+
+		let aux = this.slices + 1;
+
+		for(let j = 0; j < this.stacks; j++) {
 			for (let i = 0; i < this.slices; i++) {
-				this.vertices.push((inc + delta) * Math.cos(ang), (inc + delta) * Math.sin(ang), z + zDelta);
-				this.vertices.push(inc * Math.cos(ang), inc * Math.sin(ang), z);
-
-				this.normals.push(Math.cos(ang), Math.sin(ang), 0);
-				this.normals.push(Math.cos(ang), Math.sin(ang), 0);
-
-				ang += angDelta;
-
+				
+				this.indices.push(aux*j+i, aux*(j+1) + i, aux*j+i+1);
+				this.indices.push(aux*j+i+1, aux*(j+1)+i, aux*(j+1)+i+1);
+				this.indices.push(aux*j+i, aux*j+i+1, aux*(j+1)+i);
+				this.indices.push(aux*j+i+1, aux*(j+1)+i+1, aux*(j+1)+i);
 			}
-
-			z += zDelta;
-
-			let aux = 2 * j * this.slices;
-
-			for (let i = aux; i < aux + 2 * this.slices - 2; i += 2) {
-				this.indices.push(i, i + 1, i + 2);
-				this.indices.push(i + 2, i + 1, i + 3);
-			}
-
-			this.indices.push(aux + 2 * this.slices - 1, aux + 1, aux);
-			this.indices.push(aux + 2 * this.slices - 2, aux + 2 * this.slices - 1, aux);
 		}
-
-		/*
-		var s = 0;
-		var t = 0;
-		var s_inc = 1 / this.slices;
-		var t_inc = 1 / this.stacks;
-		for (var i = 0; i <= this.stacks; i++) {
-			for (var j = 0; j < this.slices; j++) {
-				this.texCoords.push(s, t);
-				s += s_inc;
-			}
-			s = 0;
-			t += t_inc;
-		}
-*/
+	
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 
