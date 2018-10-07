8cd14914db094e697d1e714235714383ea2bb69d
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2018-10-07 16:51:57 +0100

    Auto stash before merge of "master" and "origin/master"

--- demo/primitives/NoBaseCylinder.js
+++ demo/primitives/NoBaseCylinder.js
@@ -18,6 +18,7 @@ class NoBaseCylinder extends CGFobject {
 		this.vertices = [];
 		this.indices = [];
 		this.normals = [];
+		//this.texCoords = [];
 
 		let ang = 0;
 		let angDelta = Math.PI * 2 / this.slices;
@@ -56,20 +57,20 @@ class NoBaseCylinder extends CGFobject {
 			this.indices.push(aux + 2 * this.slices - 2, aux + 2 * this.slices - 1, aux);
 		}
 
-
-		/* let s = 0;
-		let t = 0;
-		let s_inc = 1 / this.slices;
-		let t_inc = 1 / this.stacks;
-		for (let i = 0; i <= this.stacks; i++) {
-			for (let j = 0; j < this.slices; j++) {
+		/*
+		var s = 0;
+		var t = 0;
+		var s_inc = 1 / this.slices;
+		var t_inc = 1 / this.stacks;
+		for (var i = 0; i <= this.stacks; i++) {
+			for (var j = 0; j < this.slices; j++) {
 				this.texCoords.push(s, t);
 				s += s_inc;
 			}
 			s = 0;
 			t += t_inc;
-
-		} */
+		}
+*/
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 
