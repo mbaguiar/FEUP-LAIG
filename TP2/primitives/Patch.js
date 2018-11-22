734ca1f3f407ea63532a19a865c820617597baf7
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2018-11-22 22:08:47 +0000

    Fixed Patch spaghetti

--- TP2/primitives/Patch.js
+++ TP2/primitives/Patch.js
@@ -5,32 +5,26 @@
 class Patch extends CGFobject {
     constructor(scene, npointsU, npointsV, npartsU, npartsV, controlPoints) {
         super(scene);
-        this.controlPoints = [];
-        this.makeSurface(npartsU, npartsV, npointsU, npointsV, controlPoints);
-        this.patch = new CGFnurbsObject(scene, npartsU, npartsV, this.surface);
-        console.log(this.patch);
+        let surface = this.makeSurface(npointsU, npointsV, controlPoints);
+        this.patch = new CGFnurbsObject(scene, npartsU, npartsV, surface);
     };
 
-    makeSurface(npartsU, npartsV, npointsU, npointsV, controlPoints) {
+    makeSurface(npointsU, npointsV, controlPoints) {
         let degree1 = npointsU - 1;
         let degree2 = npointsV - 1;
-        controlPoints.forEach(function(element) {
-            element.push(1);
-        });
-
+        let ctrlPoints = [];
+        
         let i = 0;
-
         for (let u = 1; u <= npointsU; u++) {
             let pointsU = [];
             for (let v = 1; v <= npointsV; v++) {
+                controlPoints[i].push(1.0);
                 pointsU.push(controlPoints[i]);
                 i++;
             }
-            this.controlPoints.push(pointsU);
+            ctrlPoints.push(pointsU);
         }
-
-        console.log(this.controlPoints);
-        this.surface = new CGFnurbsSurface(degree1, degree2, this.controlPoints);
+        return new CGFnurbsSurface(degree1, degree2, ctrlPoints);
     };
 
     display() {
