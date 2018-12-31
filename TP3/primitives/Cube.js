f0373e53b776c4b40f0ac36a9226bf68a4045e7b
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2018-12-31 17:41:58 +0000

    Changed dispenser geometry

--- TP3/primitives/Cube.js
+++ TP3/primitives/Cube.js
@@ -20,7 +20,7 @@ class Cube {
 			this.square.display();
 		this.scene.popMatrix();
 		this.scene.pushMatrix();
-			this.scene.translate(0.5, 0, 0);
+			this.scene.translate(-0.5, 0, 0);
 			this.scene.rotate(-Math.PI/2, 0, 1, 0);
 			this.square.display();
 		this.scene.popMatrix();
--- TP3/primitives/Dispenser.js
+++ TP3/primitives/Dispenser.js
@@ -6,8 +6,30 @@ class Dispenser {
 
 	display() {
 		this.scene.pushMatrix();
-			this.scene.scale(2.5, 2, 2.5);
+			this.scene.translate(0, 0.25, 0);
+			this.scene.scale(10, 0.5, 5);
 			this.cube.display();
 		this.scene.popMatrix();
+		this.scene.pushMatrix();
+			this.scene.translate(2.5, 1.5, 2.25);
+			this.scene.scale(5, 3, 0.5);
+			this.cube.display();
+		this.scene.popMatrix();
+		this.scene.pushMatrix();
+			this.scene.translate(2.5, 1.5, -2.25);
+			this.scene.scale(5, 3, 0.5);
+			this.cube.display();
+		this.scene.popMatrix();
+		this.scene.pushMatrix();
+			this.scene.translate(4.75, 1.5, 0);
+			this.scene.scale(0.5, 3, 5);
+			this.cube.display();
+		this.scene.popMatrix();
+		this.scene.pushMatrix();
+			this.scene.translate(2.5, 3, 0);
+			this.scene.scale(5, 0.5, 5);
+			this.cube.display();
+		this.scene.popMatrix();
+			
 	}
 }
